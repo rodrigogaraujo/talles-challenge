1c9aa10a1cbc2d05e6fba4d9e83f193058bdfa22
Rodrigo Araujo <rodrigoaraujo990@gmail.com>
2023-10-16 14:34:23 -0300

    add sort

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,23 +1,49 @@
 import { Table, TableCell, TableHeader, TableRow } from "./style";
 import { Transaction } from "../../models/Transaction";
 import { formatCurrency } from "../../utils";
+import { useState } from "react";
 
 interface TableDataProps {
   data: Transaction[];
 }
 function TableData({ data }: TableDataProps) {
+  const [sortedValues, setSortedValues] = useState(data);
+
+  const handleSort = (typeSort: "date" | "amount") => {
+    const sorted = [...sortedValues].sort((a, b) => {
+      if (typeSort === "date") {
+        const dateA = new Date(a.date);
+        const dateB = new Date(b.date);
+        return dateB.getTime() - dateA.getTime();
+      } else {
+        return b.amount - a.amount;
+      }
+    });
+    setSortedValues(sorted);
+  };
+
   return (
     <Table>
       <thead>
         <tr>
           <TableHeader>ID</TableHeader>
-          <TableHeader>Date</TableHeader>
+          <TableHeader
+            style={{ cursor: "pointer" }}
+            onClick={() => handleSort("date")}
+          >
+            Amount
+          </TableHeader>
           <TableHeader>Title</TableHeader>
-          <TableHeader>Amount</TableHeader>
+          <TableHeader
+            style={{ cursor: "pointer" }}
+            onClick={() => handleSort("amount")}
+          >
+            Amount
+          </TableHeader>
         </tr>
       </thead>
       <tbody>
-        {data.map((transaction, i) => (
+        {sortedValues.map((transaction, i) => (
           <TableRow hasBg={i % 2 !== 0} key={transaction.id}>
             <TableCell>{transaction.id}</TableCell>
             <TableCell>{transaction.date}</TableCell>
